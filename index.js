f0b1dd8898e6cfb78f6462e198945af6cc4068ef
Ahsan Habib <your_email@example.com>
2025-11-10 21:37:02 +0600

    add get method

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello Wodsfj!')
-})
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// MongoDB URI
+const uri = "mongodb+srv://kirishi-link:aIctiiro3ghFyRZK@cluster0.mvbamfj.mongodb.net/krishiLinkDB?retryWrites=true&w=majority";
+
+// Create MongoClient
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// GET all crops
+app.get('/crops', async (req, res) => {
+  try {
+    await client.connect();
+    const database = client.db('kirishiLink'); // your database name
+    const cropsCollection = database.collection('product'); // your collection name
+    const crops = await cropsCollection.find({}).toArray(); // get all documents
+    res.send(crops);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Failed to fetch crops' });
+  } finally {
+   
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
